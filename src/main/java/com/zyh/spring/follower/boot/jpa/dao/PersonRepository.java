2da1b82c019e650af238a1e133dde707f57cfdb9
yuhang.zhao <yuhang.zhao@ipinyou.com>
2016-03-13 20:31:20 +0800

    init

---
+++ src/main/java/com/zyh/spring/follower/boot/jpa/dao/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.zyh.spring.follower.boot.jpa.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.zyh.spring.follower.boot.jpa.model.Person;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> findByLastName(@Param("name") String name);
+
+}
